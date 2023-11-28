189a24dc52b5880ad00e46db30c988f35e50b32c
raulwad13 <rawad13@myyahoo.com>
2023-11-28 19:47:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Ej.1 -Crear la clase Persona, con propiedades nombre, edad y género, y el método obtDetalles(), que muestra por pantalla las propiedades de la persona.
+
+const persona = {
+    nombre: "Raul",
+    edad:26,
+    género:"M"
+}
+
+
+//  Ej.2 - Crear la clase Estudiante, que hereda de Persona, e incluye las propiedades curso y grupo y el método registrar(), que muestre por pantalla el resultado.
+
+
+
+
+//  Ej. 3 -Crear la clase Profesor, que hereda de Persona, e incluye las propiedades asignatura y nivel y el método asignar(), que muestre por pantalla el resultado.
+
+
+
+
+//  Ej. 4 -Crear los objetos y casos de prueba necesarios para comprobar el correcto funcionamiento de la jerarquía de clases.
+
