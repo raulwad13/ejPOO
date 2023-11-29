81c713b66cd1db9eb305f8ec35bc9e1ce13ec12e
raulwad13 <rawad13@myyahoo.com>
2023-11-29 09:57:54 +0100

    fin ej 1 y 2

--- script.js
+++ script.js
@@ -45,3 +45,52 @@ console.log(profe);
 
 // Ej. 2
 
+class warrior {
+    constructor(life,power){
+        this.life=life
+        this.power=power
+    }
+    attack(){
+        return this.power
+    }
+    defend(attack){
+        let PV = Math.abs(this.life-attack)
+        return PV 
+    }
+
+}
+
+class Maya extends warrior{
+    constructor(life,power){
+        super(life,power)
+    }
+    drinkColaCao(){
+        let healthC=this.life+10
+        return healthC
+    }
+}
+
+class Azteca extends warrior{
+    constructor(life,power){
+        super(life,power)
+    }
+    drinkNesquik(){
+        let healthN=this.life+10
+        return healthN
+    }
+}
+const maya1= new Maya(40,30)
+const azteca1= new Azteca(50,20)
+const aztecaHealth= azteca1.drinkNesquik()
+const mayaHealth= maya1.drinkColaCao()
+const firstAttack=maya1.attack(azteca1.defend())
+const secondAttack=azteca1.attack(maya1.defend)
+
+console.log(maya1);
+console.log(azteca1);
+console.log(`Azteca aumentó su vida a ${aztecaHealth}`);
+console.log(`Maya aumentó su vida a ${mayaHealth}`);
+console.log(`Azteca hizo ${firstAttack} puntos de daño`);
+console.log(`Maya hizo ${secondAttack} puntos de daño`);
+
+
